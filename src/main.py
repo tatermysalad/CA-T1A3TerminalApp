35275c94aee89116ac78fd7be7359bfd14b5f11d
tatermysalad <rosette.rural0c@icloud.com>
2023-04-27 15:36:33 +1000

    added search function with prioritised items

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
-import requests  # https://pypi.org/project/requests/
+from meal_mate_functions import add_ingr, remove_ingr, prior_ingr, view_ingr, get_recipes
+from colored import fg, bg, attr  # https://pypi.org/project/colored/
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import cm
+# c = canvas.Canvas('ex.pdf')
+# c.drawImage('ar.jpg', 0, 0, 10*cm, 10*cm)
+# c.showPage()
+# c.save()  # https://www.reportlab.com/docs/reportlab-userguide.pdf
+# https://stackoverflow.com/questions/2252726/how-to-create-pdf-files-in-python
 
-print("Welcome to Meal Mate \n'The recipe finder for your pantry items you don't know what to with!'")
+print(f"{fg(64)}Welcome to Meal Mate{attr(0)} \n{fg(18)}'The recipe finder for your pantry items you don't know what to with!' {attr(0)}")
 
 # Check if pantry_items.csv exists
 file_name = "./pantry_list.csv"
@@ -14,10 +22,41 @@ except FileNotFoundError as e:
     meal_mate_file.write("Ingredient,completed\n")
     meal_mate_file.close()
 
-def get_recipes(ingr):
-    r = requests.get(
-        'https://api.edamam.com/api/recipes/v2?type=public&q=' + ingr + '&app_id=fbfe9bd1&app_key=0f3153405e76a862a72d02b718d7e722')  # , query={'app_id': 'fbfe9bd1', 'app_key': '0f3153405e76a862a72d02b718d7e722'})
-    # r.headers['content-type'] = 'application/json; charset=utf8'
-    return r.json()
 
-print(get_recipes("meatball, chicken, pasta, lamb")["hits"][0]["recipe"]["label"])
+def create_menu():
+    print(f"{fg(2)}1.{attr(0)} Enter 1 to {fg(2)}add{attr(0)} a new item to your list")
+    print(f"{fg(1)}2.{attr(0)} Enter 2 to {fg(1)}remove{attr(0)} item from your list")
+    print(f"{fg(90)}3.{attr(0)} Enter 3 to {fg(90)}prioritise{attr(0)} an item on your list")
+    print(f"{fg(28)}4.{attr(0)} Enter 4 to {fg(28)}view{attr(0)} you ingredient list")
+    print(f"{fg(111)}5.{attr(0)} Enter 5 to {fg(111)}search{attr(0)} for recipes")
+    print(f"{fg(2)}6.{attr(0)} Enter 6 to {fg(2)}exit{attr(0)}")
+    # local variable
+    choice = input("Enter your selection: ")
+    return choice
+
+
+# global variable
+user_choice = ""
+
+while user_choice != "6":
+    user_choice = create_menu()
+
+    match user_choice:
+        case "1":
+            add_ingr(file_name)
+        case "2":
+            remove_ingr(file_name)
+        case "3":
+            prior_ingr(file_name)
+        case "4":
+            view_ingr(file_name)
+        case "5":
+            get_recipes(file_name)
+        case "6":
+            continue
+        case _:
+            print("Invalid input")
+
+    input(f"{bg(177)}Press Enter to continue...{attr(0)}")
+
+print(f"{bg(2)}Thank you for using Meal Mate{attr(0)}")
--- src/meal_mate_functions.py
+++ src/meal_mate_functions.py
@@ -1,3 +1,95 @@
 import csv
+import requests  # https://pypi.org/project/requests/
+from colored import fg, bg, attr
 
 
+def add_ingr(file_name):
+    print(f"{fg(2)}Add item{attr(0)}")
+    ingr_title = input("Enter your ingredient: ")
+    with open(file_name, "a") as ingr_file:
+        writer = csv.writer(ingr_file)
+        writer.writerow([ingr_title, "False"])
+
+
+def remove_ingr(file_name):
+    print(f"{fg(1)}Remove item{attr(0)}")
+    view_ingr(file_name)
+    ingr_title = input("Enter the ingredient that you want to remove: ")
+    ingr_lists = []
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if (ingr_title != row[0]):
+                ingr_lists.append(row)
+    # print(ingr_lists[1:])
+    # We will write that down in the file again
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(ingr_lists)
+    view_ingr(file_name)
+    
+
+
+def prior_ingr(file_name):
+    print(f"{fg(90)}Mark item to prioritise{attr(0)}")
+    view_ingr(file_name)
+    ingr_title = input("Enter the item that you want to prioritise for use")
+    ingr_lists = []
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if (ingr_title == row[0]):
+                ingr_lists.append([row[0], "True"])
+            else:
+                ingr_lists.append(row)
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(ingr_lists)
+    view_ingr(file_name)
+
+
+def view_ingr(file_name):
+    print(f"{fg(28)}View items{attr(0)}") # dark green color
+    with open(file_name, "r") as f: 
+        reader = csv.reader(f)
+        reader.__next__()
+        for row in reader:
+            if row[1] == "True":
+                print(f"{row[0]} {fg(58)}(prioritised){attr(0)}") # orange 
+            else:
+                print(f"{row[0]}")
+
+
+
+
+def get_recipes(file_name):
+    print(f"{fg(111)}Searching for recipes...{attr(0)}")
+    c = ""
+    prior = ""
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        reader.__next__()
+        for row in reader:
+            c = c + row[0] + ","
+            if row[1] == "True":
+                prior = prior + row[0] + ","
+                print(prior)
+        
+    r = requests.get(
+        'https://api.edamam.com/api/recipes/v2?type=public&q=' + c + '&app_id=fbfe9bd1&app_key=0f3153405e76a862a72d02b718d7e722')
+    # r.headers['content-type'] = 'application/json; charset=utf8'
+    json = r.json()
+    # recipes = []
+    if len(json["hits"]) < 0:
+        for recipe in json["hits"]:
+            print(recipe["recipe"]["label"])
+    else:
+            r=requests.get(
+        'https://api.edamam.com/api/recipes/v2?type=public&q=' + prior + '&app_id=fbfe9bd1&app_key=0f3153405e76a862a72d02b718d7e722')
+            json = r.json()
+            for recipe in json["hits"]:
+                print(recipe["recipe"]["label"])
+            print("no recipes found")
+
+    # print(recipes)
+        
